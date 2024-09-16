271103ef38d380405bfe04595c9749dc7025be4f
Ayarije <105593955+Ayarije@users.noreply.github.com>
2024-09-16 22:24:25 +0200

    Create prioforever.js
    Initial commit

---
+++ prioforever.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         PrioForever
+// @namespace    prioforever
+// @version      2024-09-16
+// @description  Mange en prio tt les jours
+// @author       Kilt
+// @match        *://*.index-education.net/pronote/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+function initMobile() {
+    // Get all the courses of the day
+    var liste = document.querySelectorAll(".liste-cours > li");
+    for (var i = 0; i < liste.length; i++) { // iterate over all courses
+        
+        // get the hours of the course
+        var li = liste[i];
+        var hours = li.querySelectorAll(".container-heures div");
+        
+        // if the hours are 12h10 and 13h55
+        if (hours.length == 2 && hours[0].textContent == "12h10" && hours[1].textContent == "13h55") {
+            // add the class to the schedule
+            let liste_cours = document.querySelector(".liste-cours");
+
+            let classe = document.createElement("li");
+            classe.className = "flex-contain";
+            classe.setAttribute("tabindex", "0");
+
+            classe.innerHTML = '<span class="sr-only">de 12h10 à 13h00 ACCOMPAGNEMT. PERSO.</span><div class="container-heures" aria-hidden="true"><div>13h00</div><div>13h55</div></div><div class="trait-matiere" style="background-color :#EC6719;"></div><ul class="container-cours "><li class="libelle-cours flex-contain" aria-hidden="true">ACCOMPAGNEMT. PERSO.</li><li>DU PEYRAT A.</li><li>C129 MATHS</li></ul>';
+
+            liste_cours.insertBefore(classe, li.nextSibling);
+            hours[1].textContent = "13h00"; // change the hours to 13h00
+            break;
+        } 
+    }
+}
+setInterval(initMobile, 1000);
