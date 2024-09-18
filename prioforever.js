eac9398ccefb764ebafded3c5912fae4c1923148
Ayarije <105593955+Ayarije@users.noreply.github.com>
2024-09-18 09:29:49 +0200

    Update prioforever.js
    Bug fix

--- prioforever.js
+++ prioforever.js
@@ -32,20 +32,15 @@ function initMobile() {
             break;
         }
         else if (hours.length == 2 && hours[0].textContent == "12h10" && hours[1].textContent == "13h00" && li.classList.contains("cours-annule")) {
-            li.innerHTML = '<span class="sr-only">de 12h10 à 13h55 Pause Déjeuner</span><div class="container-heures" aria-hidden="true"><div>12h10</div><div>13h00</div></div><div class="trait-matiere"></div><ul class="container-cours demi-pension" aria-label="Pause Déjeuner"><li class="libelle-cours flex-contain" aria-hidden="true"></li></ul>'
+            li.innerHTML = '<span class="sr-only">de 12h10 à 13h00 Pause Déjeuner</span><div class="container-heures" aria-hidden="true"><div>12h10</div><div>13h00</div></div><div class="trait-matiere"></div><ul class="container-cours demi-pension" aria-label="Pause Déjeuner"><li class="libelle-cours flex-contain" aria-hidden="true"></li></ul>'
             li.classList.add("greyed");
 
             // add the class to the schedule
-            let liste_cours = document.querySelector(".liste-cours");
-
-            let classe = document.createElement("li");
-            classe.className = "flex-contain";
-            classe.setAttribute("tabindex", "0");
-
-            classe.innerHTML = '<span class="sr-only">de 13h00 à 13h55 ACCOMPAGNEMT. PERSO.</span><div class="container-heures" aria-hidden="true"><div>13h00</div><div>13h55</div></div><div class="trait-matiere" style="background-color :#EC6719;"></div><ul class="container-cours "><li class="libelle-cours flex-contain" aria-hidden="true">ACCOMPAGNEMT. PERSO.</li><li>DU PEYRAT A.</li><li>C129 MATHS</li></ul>';
-
-            liste_cours.insertBefore(classe, li.nextSibling);
-            hours[1].textContent = "13h00"; // change the hours to 13h00
+            if (liste.length > i+1)
+            {
+                liste[i+1].innerHTML = '<span class="sr-only">de 13h00 à 13h55 ACCOMPAGNEMT. PERSO.</span><div class="container-heures" aria-hidden="true"><div>13h00</div><div>13h55</div></div><div class="trait-matiere" style="background-color :#EC6719;"></div><ul class="container-cours "><li class="libelle-cours flex-contain" aria-hidden="true">ACCOMPAGNEMT. PERSO.</li><li>DU PEYRAT A.</li><li>C129 MATHS</li></ul>';
+                liste[i+1].classList.remove("greyed");
+            }
             break;
         }
     }
